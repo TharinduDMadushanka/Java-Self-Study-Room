5ee2d23169c2543f8eb97b8b6e0aaadf6b228b3f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-04-12 23:13:08 +0530

    core java practice

---
+++ Practice/Core Practice/Q22.java
@@ -0,0 +1,13 @@
+public class Q22{
+	public static void main(String [] args){
+		
+		int [] age = {1,2,3,4,5};
+		
+		System.out.println("Pre " +age[0]);
+		System.out.println(age.length);
+		
+		age[0]=10;
+		System.out.println("Post "+age[0]);
+	}
+}
+
