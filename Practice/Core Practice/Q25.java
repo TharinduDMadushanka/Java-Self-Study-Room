2653e1919bf68f58dbfeb61373c3a986da59e670
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-04-15 22:58:11 +0530

    practice core java

---
+++ Practice/Core Practice/Q25.java
@@ -0,0 +1,10 @@
+public class Q25{
+	public static void main(String [] args){
+		System.out.println("The sum is "+myMethod(3,5));
+		}
+		public static int myMethod(int a,int b){
+			int x =a+b;
+			return x;
+			}
+	}
+
