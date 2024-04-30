4d9da4cb82494af3c33a27670c04ad75cc16dbd2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-01 03:14:35 +0530

    practice array challanges

--- Practice/Arrays/src/codingChallenges/Q1.java
+++ Practice/Arrays/src/codingChallenges/Q1.java
@@ -1,4 +1,26 @@
 package codingChallenges;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Q1 {
+    public static void main(String[] args) {
+        Random random=new Random();
+        int r =random.nextInt(101);
+
+        int[] a =new int[10];
+        int[] b =new int[10];
+
+        if (r<50){
+            for (int i=0;i<a.length;i++){
+                a[i]=r;
+            }
+        }else {
+            for (int i=0;i<b.length;i++){
+                b[i]=r;
+            }
+        }
+        System.out.println("a-->"+ Arrays.toString(a));
+        System.out.println("b-->"+ Arrays.toString(b));
+    }
 }
