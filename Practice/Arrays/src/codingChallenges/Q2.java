4d74cabb13a0e1685b16ace021bea60d752d02c4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-01 06:15:44 +0530

    practice array challanges

--- Practice/Arrays/src/codingChallenges/Q2.java
+++ Practice/Arrays/src/codingChallenges/Q2.java
@@ -1,5 +1,3 @@
-package codingChallenges;
-
 import java.util.Arrays;
 import java.util.Random;
 
@@ -7,22 +5,27 @@ public class Q2 {
     public static void main(String[] args) {
         Random random = new Random();
 
-
         int[] a = new int[10];
         int[] b = new int[10];
+        int bIndex = 0; // Index for tracking elements added to array b
 
         for (int i = 0; i < a.length; i++) {
             int r = random.nextInt(101);
 
-            if (r < 50) {
+            if (r == 0) {
+                // Add 0 to array b
+                b[bIndex++] = r;
+            } else if (r < 50) {
                 a[i] = r;
-            }else {
-                b[i]=r;
+            } else {
+                b[bIndex++] = r;
             }
         }
 
+        // Trim array b to remove any trailing 0s
+        b = Arrays.copyOf(b, bIndex);
 
-        System.out.println("a-->" + Arrays.toString(a));
-        System.out.println("b-->" + Arrays.toString(b));
+        System.out.println("a --> " + Arrays.toString(a));
+        System.out.println("b --> " + Arrays.toString(b));
     }
 }
