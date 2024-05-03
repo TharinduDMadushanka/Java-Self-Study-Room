968bd650e7b969643ffd7c7a9df466b5de4743e2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-03 20:29:50 +0530

    2d array practice

---
+++ Practice/Arrays/src/Arrays_2D/Q12.java
@@ -0,0 +1,24 @@
+package Arrays_2D;
+
+public class Q12 {
+    public static void main(String[] args) {
+
+        int[][] integers = {{1, 10, 3, 8}, {9, 12, 6, 7}, {5, 2, 11, 6}};
+
+        int min=Integer.MAX_VALUE;
+        int sum=0;
+
+        for (int j=0;j<integers[0].length;j++){
+            sum=integers[0][j];
+            for (int i=1;i<integers.length;i++){
+                sum+=integers[i][j];
+            }
+            System.out.println("The sum of column "+(j+1)+" :"+sum);
+
+            if (sum<min){
+                min=sum;
+            }
+        }
+        System.out.println("The minimum sum is "+min);
+    }
+}
