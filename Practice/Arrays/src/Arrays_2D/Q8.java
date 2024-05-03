15f27ec08d1b61a56176fe26fa88dd7f44ae725d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-03 18:52:33 +0530

    2d array practice

---
+++ Practice/Arrays/src/Arrays_2D/Q8.java
@@ -0,0 +1,32 @@
+package Arrays_2D;
+
+/**
+ * Maximum Element
+ * Write a Java program to find the maximum element in a 2D integer array and print its position (row and column).
+ */
+
+public class Q8 {
+    public static void main(String[] args) {
+
+        int[][] integers = {{1, 10, 3, 8}, {9, 12, 6, 7}, {5, 2, 11, 4}};
+
+        int all=0;
+
+        for (int i = 0; i < integers.length; i++) {
+            int max = integers[i][0];
+            for (int j = 0; j < integers[i].length; j++) {
+                if (max < integers[i][j]) {
+                    max = integers[i][j];
+                }
+            }
+            System.out.println("The maximum number of row "+(i+1)+" :\t" + max);
+
+            for (int z=0;z<integers.length;z++){
+                if (all<max){
+                    all=max;
+                }
+            }
+        }
+        System.out.println("Max element :"+all);
+    }
+}
