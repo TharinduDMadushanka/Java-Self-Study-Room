610f98331b6aa42d8bdc9d7d235a28374e35897f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-20 11:16:32 +0530

    first commit

--- CMJD/Assignments/OOP_AS_01/src/Q17.java
+++ CMJD/Assignments/OOP_AS_01/src/Q17.java
@@ -1,17 +1,17 @@
-class Q17{
-    int a=10;
-    static int b=20;
-    public static void main(String args[]){
-        int c=20;
-        Demo ob=new Demo();
-        System.out.println(a); //Line 1
-        System.out.println(b); //Line 2
-        System.out.println(c); //Line 3
-        System.out.println(Demo.a); //Line 4
-        System.out.println(Demo.b); //Line 5
-        System.out.println(Demo.c); //Line 6
-        System.out.println(ob.a); //Line 7
-        System.out.println(ob.b); //Line 8
-        System.out.println(ob.c); //Line 9
-    }
-}
+//class Q17{
+//    int a=10;
+//    static int b=20;
+//    public static void main(String args[]){
+//        int c=20;
+//        Demo ob=new Demo();
+//        System.out.println(a); //Line 1
+//        System.out.println(b); //Line 2
+//        System.out.println(c); //Line 3
+//        System.out.println(Demo.a); //Line 4
+//        System.out.println(Demo.b); //Line 5
+//        System.out.println(Demo.c); //Line 6
+//        System.out.println(ob.a); //Line 7
+//        System.out.println(ob.b); //Line 8
+//        System.out.println(ob.c); //Line 9
+//    }
+//}
