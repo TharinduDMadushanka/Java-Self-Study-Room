91d7cda612fd5dde9423ae43723aae52051fe458
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-20 10:21:00 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_01/src/Q11.java
@@ -0,0 +1,13 @@
+class A1{
+    int a;
+    //A1(int i){a=i;}
+    void A1(int i){a=i;}
+    A1(){}
+
+//Line 12
+}
+class Q11{
+    public static void main(String args[]){
+        A1 a2=new A1();
+    }
+}
