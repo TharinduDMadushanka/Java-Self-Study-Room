b737f63be3eb9beff4a50b61e16bcf427d6a0ab6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-20 09:52:49 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_01/src/Q3.java
@@ -0,0 +1,13 @@
+class Box{
+    int length;
+    int width;
+    int height;
+}
+class Demo{
+    public static void main(String args[]){
+        Box b1=new Box(); // 1
+//        System.out.println("length of box : "+length); //2
+//        System.out.println("width of box : "+width); //3
+//        System.out.println("height of box : "+height); //4
+    }
+}
