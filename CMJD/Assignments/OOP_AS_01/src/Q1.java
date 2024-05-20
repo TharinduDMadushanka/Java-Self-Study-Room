420d167c400cf8aa593f74116649446016e634c5
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-20 09:47:31 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_01/src/Q1.java
@@ -0,0 +1,12 @@
+//--------------------Test.java------------------------
+class Test{
+    int a=8;
+    int b=9;
+}
+//--------------------Demo.java------------------------
+class Q1{
+    public static void main(String args[]){
+//        System.out.print(a);
+//        System.out.println(b); ---> given compile error
+    }
+}
