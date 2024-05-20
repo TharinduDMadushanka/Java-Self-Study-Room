fb0e0b8fc658cf25b5981fa04541828e1814a843
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-20 11:30:50 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_01/src/Q22.java
@@ -0,0 +1,12 @@
+//class Q22{
+//    int x=10;
+//    static int y=20;
+//    void mA(){
+//        System.out.println(this.x); //Line 1
+//        System.out.println(this.y); //Line 2
+//    }
+//    static void mB(){
+//        System.out.println(this.x); //Line 3
+//        System.out.println(this.y); //Line 4
+//    }
+//}
