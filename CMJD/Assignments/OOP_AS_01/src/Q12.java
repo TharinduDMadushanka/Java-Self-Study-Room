2dbb576dab5c7af16c2dd16a98c593dd61f9ab29
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-20 10:56:00 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_01/src/Q12.java
@@ -0,0 +1,9 @@
+//class Test{
+//    int a=100; // Line 1
+//    System.out.print(a); //Line 2
+//}
+//class DemoTest{
+//    public static void main(String args[]){
+//        Test t=new Test();
+//    }
+//}
