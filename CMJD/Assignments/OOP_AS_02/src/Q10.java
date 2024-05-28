aee12442154c3e989d38c7c3d3b91e12df98bf27
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 11:07:07 +0530

    first commit

--- CMJD/Assignments/OOP_AS_02/src/Q10.java
+++ CMJD/Assignments/OOP_AS_02/src/Q10.java
@@ -1,5 +1,40 @@
 /**
  * Q13
  */
-public class Q10 {
+import javax.swing.*;
+import java.awt.*;
+
+public class Q10 extends JFrame {
+    public void CalculatorUI() {
+        setTitle("Calculator");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel displayPanel = new JPanel();
+        JTextField display = new JTextField(20);
+        display.setHorizontalAlignment(JTextField.RIGHT);
+        displayPanel.add(display);
+        add(displayPanel, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4));
+
+        String[] buttons = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"
+        };
+
+        for (String text : buttons) {
+            buttonPanel.add(new JButton(text));
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
+    }
+
+    public void main(String[] args) {
+        setVisible(true);
+    }
 }
