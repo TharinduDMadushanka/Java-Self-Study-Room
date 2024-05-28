4268556461afc576cbb06ace8a1c1bb186bf8719
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 11:27:19 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_02/src/Q28.java
@@ -0,0 +1,28 @@
+//import java.awt.*;
+//import javax.swing.*;
+//
+//class SliderExample extends JFrame {
+//    private JSlider volumeSlider;
+//
+//    SliderExample() {
+//        setSize(300, 100);
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        setTitle("JSlider Example");
+//        setLayout(new FlowLayout());
+//
+//        // Initialize the slider
+//        volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+//        volumeSlider.setMajorTickSpacing(10);
+//        volumeSlider.setPaintTicks(true);
+//        volumeSlider.setPaintLabels(true);
+//
+//        // Add the slider to the frame
+//        add(volumeSlider);
+//    }
+//}
+//
+//class Demo {
+//    public static void main(String args[]) {
+//        new SliderExample().setVisible(true);
+//    }
+//}
