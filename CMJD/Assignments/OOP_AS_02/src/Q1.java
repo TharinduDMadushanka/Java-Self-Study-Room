7c64c0fcbada90488a114d83973e12521dcd753a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 10:45:15 +0530

    first commit

--- CMJD/Assignments/OOP_AS_02/src/Q1.java
+++ CMJD/Assignments/OOP_AS_02/src/Q1.java
@@ -14,14 +14,9 @@ public class Q1 {
         JFrame f1 = new JFrame();
 
         f1.setSize(300, 300);
-        f1.setTitle("My Frame");
+        f1.setTitle("My First Frame");
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f1.setLocationRelativeTo(null);
-
-        JButton b = new JButton(); // create object for create button
-        b.setText("Click");
-        b.setFont(new Font("",1,25));
-        f1.add(b);
 
         f1.setVisible(true);
 
