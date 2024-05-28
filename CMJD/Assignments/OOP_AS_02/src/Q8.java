db314113fa3868d809e929fd2de5bc57cb2ee300
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 10:59:24 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_02/src/Q8.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GridLayoutExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GridLayout Example");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(2, 2));
+
+        frame.add(new JButton("Button1"));
+        frame.add(new JButton("Button2"));
+        frame.add(new JButton("Button3"));
+        frame.add(new JButton("Button4"));
+
+        frame.setVisible(true);
+    }
+}
