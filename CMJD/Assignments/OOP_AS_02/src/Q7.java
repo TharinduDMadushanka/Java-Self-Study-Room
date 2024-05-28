17a1aef1320897c405947104ef4aea7422ae7990
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 10:58:53 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_02/src/Q7.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Q7 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Right Alignment FlowLayout");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        frame.add(new JButton("Button1"));
+        frame.add(new JButton("Button2"));
+        frame.add(new JButton("Button3"));
+        frame.add(new JButton("Button4"));
+
+        frame.setVisible(true);
+    }
+}
