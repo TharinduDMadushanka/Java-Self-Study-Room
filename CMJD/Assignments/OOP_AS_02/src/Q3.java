ffcc018aa8e76aa22f30e9929d8c928a82c6f9a3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 10:55:14 +0530

    first commit

--- CMJD/Assignments/OOP_AS_02/src/Q3.java
+++ CMJD/Assignments/OOP_AS_02/src/Q3.java
@@ -1,2 +1,18 @@
-public class Q3 {
+import javax.swing.*;
+import java.awt.*;
+
+public class AnotherBorderLayoutWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Border Layout Window");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel northLabel = new JLabel("North Label", JLabel.CENTER);
+        JLabel southLabel = new JLabel("South Label", JLabel.CENTER);
+
+        frame.add(northLabel, BorderLayout.NORTH);
+        frame.add(southLabel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
 }
