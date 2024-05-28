e9d7ce8294e0df735c9c81c61f33fddd43e0b443
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-28 11:26:42 +0530

    first commit

---
+++ CMJD/Assignments/OOP_AS_02/src/Q26.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import javax.swing.*;
+
+class CustomUI extends JFrame {
+    private JButton button1, button2;
+    private JTextField textField;
+
+    CustomUI() {
+        setSize(300, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Custom UI");
+        setLayout(new FlowLayout());
+
+        // Initialize components
+        textField = new JTextField(15);
+        button1 = new JButton("Button 1");
+        button2 = new JButton("Button 2");
+
+        // Add components to the frame
+        add(textField);
+        add(button1);
+        add(button2);
+    }
+}
+
+class Demo {
+    public static void main(String args[]) {
+        new CustomUI().setVisible(true);
+    }
+}
