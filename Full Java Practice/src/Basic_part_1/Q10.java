c10b4bf69b5003c7c1a43a2f0453266abd7418ef
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 09:30:10 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q10.java
+++ Full Java Practice/src/Basic_part_1/Q10.java
@@ -1,2 +1,28 @@
-package Basic_part_1;public class Q10 {
+package Basic_part_1;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program to compare two numbers
+ */
+public class Q10 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter 1st nb :");
+        int nb1= input.nextInt();;
+
+        System.out.print("Enter 2nd nb :");
+        int nb2 = input.nextInt();
+
+        if (nb1==nb2){
+            System.out.println(nb1+" = "+nb2);
+        } else if (nb1<nb2) {
+            System.out.println(nb1+" < "+nb2);
+        } else if (nb1>nb2) {
+            System.out.println(nb1+" > "+nb2);
+        }else {
+            System.out.println(nb1+" != "+nb2);
+        }
+    }
 }
