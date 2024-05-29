e09abc1f00baeb402ebeadc5f184ef3da7b5722c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 14:54:53 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q15.java
+++ Full Java Practice/src/Basic_part_1/Q15.java
@@ -1,5 +1,8 @@
 package Basic_part_1;
 
+/**
+ * 41.Write a Java program to print the ASCII value of a given character.
+ */
 public class Q15 {
     public static void main(String[] args) {
         int chr ='z';
---
+++ Full Java Practice/src/Basic_part_1/Q16.java
@@ -0,0 +1,9 @@
+package Basic_part_1;
+
+import java.util.Scanner;
+
+public class Q16 {
+    public static void main(String[] args) {
+
+    }
+}
