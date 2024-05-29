21f1326bbb5d453fd47220234994e67a0e4180f9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 09:40:00 +0530

    first commit

---
+++ Full Java Practice/src/Basic_part_1/Q12.java
@@ -0,0 +1,20 @@
+package Basic_part_1;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program to reverse a string.
+ */
+public class Q12 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        char[] letter =input.next().toCharArray();
+
+        System.out.println("Reverse string: ");
+
+        for (int i=letter.length-1;i<0;i--){
+            System.out.print(letter[i]);
+        }
+    }
+}
