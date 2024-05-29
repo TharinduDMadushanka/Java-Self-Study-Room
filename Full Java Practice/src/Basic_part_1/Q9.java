8af84c39a98bf0d62937271fdc90f1367ff9c536
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 08:58:53 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q9.java
+++ Full Java Practice/src/Basic_part_1/Q9.java
@@ -1,4 +1,27 @@
 package Basic_part_1;
 
+/**
+ * 31. Write a Java program to check whether Java is installed on your computer.
+ */
+
 public class Q9 {
+    public static void main(String[] args) {
+        // Display Java version
+        System.out.println("\nJava Version: " + System.getProperty("java.version"));
+
+        // Display Java runtime version
+        System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
+
+        // Display Java home directory
+        System.out.println("Java Home: " + System.getProperty("java.home"));
+
+        // Display Java vendor name
+        System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
+
+        // Display Java vendor URL
+        System.out.println("Java Vendor URL: " + System.getProperty("java.vendor.url"));
+
+        // Display Java class path
+        System.out.println("Java Class Path: " + System.getProperty("java.class.path") + "\n");
+    }
 }
