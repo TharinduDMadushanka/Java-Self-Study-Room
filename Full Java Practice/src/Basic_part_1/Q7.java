eb551b3f9d30ed10d2733387a1945dcdd86ea7ef
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 08:49:07 +0530

    first commit

---
+++ Full Java Practice/src/Basic_part_1/Q7.java
@@ -0,0 +1,8 @@
+package Basic_part_1;
+
+public class Q7 {
+    public static void main(String[] args) {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+    }
+}
