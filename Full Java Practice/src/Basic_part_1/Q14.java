c821edaec078696af7409e20751ee79ba4e8f2c7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 14:45:30 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q14.java
+++ Full Java Practice/src/Basic_part_1/Q14.java
@@ -1,7 +1,7 @@
 package Basic_part_1;
 
 /**
- * Write a Java program to create and display a unique three-digit number using 1, 2, 3, 4. Also count how many three-digit numbers are there.
+ * 39.Write a Java program to create and display a unique three-digit number using 1, 2, 3, 4. Also count how many three-digit numbers are there.
  */
 public class Q14 {
     public static void main(String[] args) {
