cea7c47284ba40939f0834b58dd4639b6f0f65c7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 08:36:47 +0530

    first commit

---
+++ Full Java Practice/src/Basic_part_1/Q1.java
@@ -0,0 +1,10 @@
+/**
+ * 1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+ */
+package Basic_part_1;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
