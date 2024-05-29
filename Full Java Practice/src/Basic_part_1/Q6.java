d46e41898f7c84a2031a1a648661fb991b6e4f58
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 08:48:07 +0530

    first commit

---
+++ Full Java Practice/src/Basic_part_1/Q6.java
@@ -0,0 +1,10 @@
+package Basic_part_1;
+
+public class Q6 {
+    public static void main(String[] args) {
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+}
