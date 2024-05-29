0be5dac7e04df139de38cdbf74640c1530073e94
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 08:39:38 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q3.java
+++ Full Java Practice/src/Basic_part_1/Q3.java
@@ -3,5 +3,9 @@ package Basic_part_1;
 public class Q3 {
     public static void main(String[] args) {
         System.out.println(50/3);
+        System.out.println(-5+8*6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println( 5 + 15 / 3 * 2 - 8 % 3);
     }
 }
