c83959bd175c28d6c4c6dd8e21bc235033670b21
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-29 14:27:14 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q13.java
+++ Full Java Practice/src/Basic_part_1/Q13.java
@@ -1,7 +1,23 @@
 package Basic_part_1;
 
+import java.util.Scanner;
+
 /**
  * 38. Write a Java program to count letters, spaces, numbers and other characters in an input string.
  */
 public class Q13 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter string:");
+
+        char [] letter =input.nextLine().toCharArray();
+
+        int count =0;
+
+        for (int i= letter.length-1;i>=0;i--){
+            count++;
+        }
+        System.out.println(count);
+    }
 }
