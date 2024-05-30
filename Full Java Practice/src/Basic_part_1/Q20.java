69a4ee4d884e0ecaa150fc454b0e77b45d2b57b2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-30 08:57:59 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q20.java
+++ Full Java Practice/src/Basic_part_1/Q20.java
@@ -7,8 +7,11 @@ public class Q20 {
         System.out.println("Enter 5 numbers for array:");
 
         for (int i=0;i<5;i++){
-            System.out.print("\nEnter nb "+(i+1));
+            System.out.println("Enter nb "+(i+1));
             arr[i]=i;
+        }
+        for (int a:arr){
+            System.out.print(arr[a]+", ");
         }
     }
 }
