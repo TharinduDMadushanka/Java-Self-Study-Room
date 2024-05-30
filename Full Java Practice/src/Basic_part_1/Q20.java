9b39d7773897e68bf32874e878b8ac71633cf658
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-30 09:00:16 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q20.java
+++ Full Java Practice/src/Basic_part_1/Q20.java
@@ -1,14 +1,17 @@
 package Basic_part_1;
 
+import java.util.Scanner;
+
 public class Q20 {
     public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
         int[] arr =new int[5];
 
         System.out.println("Enter 5 numbers for array:");
 
         for (int i=0;i<5;i++){
-            System.out.println("Enter nb "+(i+1));
-            arr[i]=i;
+            System.out.println("Enter nb "+(i+1)+":");
+            arr[i]=input.nextInt();
         }
         for (int a:arr){
             System.out.print(arr[a]+", ");
