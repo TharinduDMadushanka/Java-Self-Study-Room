b18b71f89df9b8c7180cc50a70fcc15a0b5dc2a9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-30 08:36:14 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q18.java
+++ Full Java Practice/src/Basic_part_1/Q18.java
@@ -6,13 +6,24 @@ package Basic_part_1;
 public class Q18 {
     public static void main(String[] args) {
 
-        for (int i=0;i<100;i++){
+        System.out.println("Divide by 3: ");
+        for (int i=1;i<100;i++){
             if (i%3==0){
-                System.out.println("Divide by 3: ");
-                System.out.println(i);
-            }else if (i%5==0){
-                System.out.println("Divide by 5: ");
-                System.out.println(i);
+                System.out.print(i+", ");
+            }
+        }
+
+        System.out.println("\nDivide by 5: ");
+        for (int i=1;i<100;i++){
+            if (i%5==0){
+                System.out.print(i+", ");
+            }
+        }
+
+        System.out.println("\nDivide by 5 and 3: ");
+        for (int i=1;i<100;i++){
+            if (i%3==0 && i%5==0){
+                System.out.print(i+", ");
             }
         }
     }
