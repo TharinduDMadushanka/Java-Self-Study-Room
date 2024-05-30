d67bdfe63ba91ad61c5e858de47c0f0af937c5e8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-30 09:40:46 +0530

    first commit

--- Full Java Practice/src/Basic_part_1/Q22.java
+++ Full Java Practice/src/Basic_part_1/Q22.java
@@ -6,10 +6,8 @@ public class Q22 {
         int [] arr2 ={3,4,5,6,7};
 
         for (int i=0;i<arr1.length;i++) {
-            for (int j=0;j< arr2.length;j++){
-                if (arr1[i]==arr2[j]){
-                    System.out.println("Arrays are similar.");
-                }
+            if (arr1[i]==arr2[i]){
+                System.out.println("same");
             }
         }
     }
