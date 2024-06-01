67c805754c1409396775044a33a94bbe7934523e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 18:20:34 +0530

    first commit

---
+++ Full Java Practice/src/Inheritance/Q3.java
@@ -0,0 +1,36 @@
+package Inheritance;
+
+/**
+ * Write a Java program to create a class called Shape with a method called getArea(). Create a subclass called Rectangle
+ * that overrides the getArea() method to calculate the area of a rectangle.
+ */
+public class Q3 {
+    public static void main(String[] args) {
+
+        Rectangle r =new Rectangle(10,20);
+
+        double area =r.getArea();
+        System.out.println(area); // 200.0
+
+        Shape s =new Shape();
+        System.out.println(s.getArea()); // 0.0
+
+    }
+}
+class Shape{
+    public double getArea(){
+        return 0.0;
+    }
+}
+class Rectangle extends Shape{
+    private double length;
+    private double height;
+
+    public Rectangle(double length, double height){
+        this.length=length;
+        this.height=height;
+    }
+    public double getArea(){
+        return length*height;
+    }
+}
