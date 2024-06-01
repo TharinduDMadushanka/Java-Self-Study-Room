893871875578f2190a6567d2919a35169a0ec0fa
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 18:13:50 +0530

    first commit

--- Full Java Practice/src/Inheritance/Q2.java
+++ Full Java Practice/src/Inheritance/Q2.java
@@ -7,5 +7,22 @@ package Inheritance;
 public class Q2 {
     public static void main(String[] args) {
 
+        Vehicle v1 =new Vehicle();
+        v1.drive(); //call Vehicle class method
+
+        Car c1=new Car();
+        c1.drive(); //call Car class method
+
+
+    }
+}
+class Vehicle{
+     public void drive(){
+        System.out.println("Vehicle drive");
+    }
+}
+class Car extends Vehicle{
+    public void drive(){
+        System.out.println("Car drive");
     }
 }
