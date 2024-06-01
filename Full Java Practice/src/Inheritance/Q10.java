1c5803c2107ee27423bfbb73881c93f45017d80c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 22:57:19 +0530

    first commit

---
+++ Full Java Practice/src/Inheritance/Q10.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+/**
+ * Write a Java program to create a vehicle class hierarchy. The base class should be Vehicle, with subclasses Truck, Car and Motorcycle. Each subclass should have properties such as make, model, year, and fuel type.
+ * Implement methods for calculating fuel efficiency, distance traveled, and maximum speed.
+ */
+public class Q10 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Full Java Practice/src/Inheritance/Q9.java
@@ -0,0 +1,4 @@
+package Inheritance;
+
+public class Q9 {
+}
