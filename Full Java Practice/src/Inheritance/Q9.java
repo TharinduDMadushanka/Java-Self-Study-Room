e1be069ac6490c8c408282f8c1a27c3cc689cbfb
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 10:53:23 +0530

    first commit

--- Full Java Practice/src/Inheritance/Q9.java
+++ Full Java Practice/src/Inheritance/Q9.java
@@ -1,4 +1,5 @@
 package Inheritance;
+
 /**
  * Write a Java program to create a vehicle class hierarchy. The base class should be Vehicle, with subclasses Truck,
  * Car and Motorcycle. Each subclass should have properties such as make, model, year, and fuel type.
@@ -7,109 +8,145 @@ package Inheritance;
 public class Q9 {
     public static void main(String[] args) {
 
-        Truck truck=new Truck("Tatra", "Tatra 810 4x4", 2020, "GASOLINE", 8.112, 4.5);
+        Truck truck = new Truck("Tatra", "Tatra 810 4x4", 2020, "GASOLINE", 8.112, 4.5);
+
         Car car = new Car("Volkswagen", "Virtus", 2019, "HYBRID", 6.123, 8);
+
         Motorcycle motorcycle = new Motorcycle("Massimo Motor", "Warrior200", 2018, "GASOLINE", 2.1);
+
+        // Print the vehicle details and calculations
+        System.out.println("Truck Model: " + truck.getModel());
+        System.out.println("Fuel Efficiency: " + truck.calculateFuelEfficiency() + " mpg");
+        System.out.println("Distance Traveled: " + truck.calculateDistanceTravelled() + " miles");
+        System.out.println("Max Speed: " + truck.getMaxSpeed() + " mph\n");
+
+        System.out.println("Car Model: " + car.getModel());
+        System.out.println("Fuel Efficiency: " + car.calculateFuelEfficiency() + " mpg");
+        System.out.println("Distance Traveled: " + car.calculateDistanceTravelled() + " miles");
+        System.out.println("Max Speed: " + car.getMaxSpeed() + " mph\n");
+
+        System.out.println("Motorcycle Model: " + motorcycle.getModel());
+        System.out.println("Fuel Efficiency: " + motorcycle.calculateFuelEfficiency() + " mpg");
+        System.out.println("Distance Traveled: " + motorcycle.calculateDistanceTravelled() + " miles");
+        System.out.println("Max Speed: " + motorcycle.getMaxSpeed() + " mph");
     }
 }
-abstract class Vehicle{
+
+abstract class Vehicle {
     private String make;
     private String model;
     private int year;
     private String fuelType;
     private double fuelEfficiency;
 
-    public Vehicle(String make,String model,int year,String fuelType,double fuelEfficiency){
-        this.make=make;
-        this.model=model;
-        this.year=year;
-        this.fuelType=fuelType;
-        this.fuelEfficiency=fuelEfficiency;
+    public Vehicle(String make, String model, int year, String fuelType, double fuelEfficiency) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.fuelType = fuelType;
+        this.fuelEfficiency = fuelEfficiency;
     }
 
-    public String getMake(){
+    public String getMake() {
         return make;
     }
-    public String getModel(){
+
+    public String getModel() {
         return model;
     }
-    public int getYear(){
+
+    public int getYear() {
         return year;
     }
-    public String getFuelType(){
+
+    public String getFuelType() {
         return fuelType;
     }
-    public double getFuelEfficiency(){
+
+    public double getFuelEfficiency() {
         return fuelEfficiency;
     }
+
     public abstract double calculateFuelEfficiency();
+
     public abstract double calculateDistanceTravelled();
+
     public abstract double getMaxSpeed();
 
 }
-class Truck extends Vehicle{
+
+class Truck extends Vehicle {
     private double cargoCapacity;
-    public Truck(String make,String model,int year,String fuelType,double fuelEfficiency,double cargoCapacity){
+
+    public Truck(String make, String model, int year, String fuelType, double fuelEfficiency, double cargoCapacity) {
         super(make, model, year, fuelType, fuelEfficiency);
-        this.cargoCapacity=cargoCapacity;
+        this.cargoCapacity = cargoCapacity;
     }
 
-    public double getCargoCapacity(){
+    public double getCargoCapacity() {
         return cargoCapacity;
     }
-    public double calculateFuelEfficiency(){
-        return getFuelEfficiency()*(1.0 / (1.0 + (getCargoCapacity() / 1000.0)));
+
+    public double calculateFuelEfficiency() {
+        return getFuelEfficiency() * (1.0 / (1.0 + (getCargoCapacity() / 1000.0)));
     }
-    public  double calculateDistanceTravelled(){
+
+    public double calculateDistanceTravelled() {
         return calculateFuelEfficiency() * getFuelEfficiency();
     }
-    public  double getMaxSpeed(){
+
+    public double getMaxSpeed() {
         return 80.0;
     }
 }
 
-class Car extends Vehicle{
+class Car extends Vehicle {
     private int numSeats;
 
-    public Car(String make,String model,int year,String fuelType,double fuelEfficiency,int numSeats){
+    public Car(String make, String model, int year, String fuelType, double fuelEfficiency, int numSeats) {
         super(make, model, year, fuelType, fuelEfficiency);
-        this.numSeats=numSeats;
+        this.numSeats = numSeats;
     }
 
-    public int getNumSeats(){
+    public int getNumSeats() {
         return numSeats;
     }
 
-    public  double calculateFuelEfficiency(){
+    public double calculateFuelEfficiency() {
         return getFuelEfficiency() * (1.0 / (1.0 + (getNumSeats() / 5.0)));
     }
-    public  double calculateDistanceTravelled(){
+
+    public double calculateDistanceTravelled() {
         return calculateFuelEfficiency() * getFuelEfficiency();
     }
-    public  double getMaxSpeed(){
+
+    public double getMaxSpeed() {
         return 120.0;
     }
 }
 
-class Motorcycle extends Vehicle{
+class Motorcycle extends Vehicle {
 
     private double engineDisplacement;
 
-    public Motorcycle(String make,String model,int year,String fuelType,double fuelEfficiency){
+    public Motorcycle(String make, String model, int year, String fuelType, double fuelEfficiency) {
         super(make, model, year, fuelType, fuelEfficiency);
         //this.engineDisplacement=engineDisplacement;
     }
-    public double getEngineDisplacement(){
+
+    public double getEngineDisplacement() {
         return engineDisplacement;
     }
 
-    public  double calculateFuelEfficiency(){
+    public double calculateFuelEfficiency() {
         return getFuelEfficiency() * (1.0 / (1.0 + (getEngineDisplacement() / 1000.0)));
     }
-    public  double calculateDistanceTravelled(){
+
+    public double calculateDistanceTravelled() {
         return calculateFuelEfficiency() * getFuelEfficiency();
     }
-    public  double getMaxSpeed(){
+
+    public double getMaxSpeed() {
         return 80.0;
     }
 }
