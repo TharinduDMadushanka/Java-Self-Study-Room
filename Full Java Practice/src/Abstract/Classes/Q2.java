40006de95f44d6f722253908b4eedf9a06730a3c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 20:52:34 +0530

    first commit

--- Full Java Practice/src/Abstract/Classes/Q2.java
+++ Full Java Practice/src/Abstract/Classes/Q2.java
@@ -28,3 +28,22 @@ class Circle extends Shape{
         return 2*Math.PI*radius;
     }
 }
+class Triangle extends Shape{
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1,double side2,double side3){
+        this.side1=side1;
+        this.side2=side2;
+        this.side3=side3;
+    }
+    public double calcArea(){
+        double s =(side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public double calcPerimeter(){
+        return side1 + side2 + side3;
+    }
+}
