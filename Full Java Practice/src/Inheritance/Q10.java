0fffd775e43e3dcf56baa4b7a93354b1ea0a96fa
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 11:00:03 +0530

    first commit

--- Full Java Practice/src/Inheritance/Q10.java
+++ Full Java Practice/src/Inheritance/Q10.java
@@ -1,8 +1,19 @@
 package Inheritance;
 
+/**
+ * Write a Java program that creates a class hierarchy for employees of a company. The base class should be Employee,
+ * with subclasses Manager, Developer, and Programmer. Each subclass should have properties such as name, address, salary,
+ * and job title. Implement methods for calculating bonuses, generating performance reports, and managing projects.
+ */
 
 public class Q10 {
     public static void main(String[] args) {
 
     }
+}
+class Employee{
+    private String name;
+    private String address;
+    private double salary;
+    private String jobTitle;
 }
--- Full Java Practice/src/Inheritance/Q7.java
+++ Full Java Practice/src/Inheritance/Q7.java
@@ -4,44 +4,44 @@ package Inheritance;
  * Write a Java program to create a class known as Person with methods called getFirstName() and getLastName(). Create a
  * subclass called Employee that adds a new method named getEmployeeId() and overrides the getLastName() method to include the employee's job title.
  */
-public class Q7 {
-    public static void main(String[] args) {
-
-        Employee employee1 = new Employee("Kortney", "Rosalee", 4451, "HR Manager");
-        System.out.println(employee1.getfName() + " " + employee1.getLastName() + " (" + employee1.geteId() + ")");
-
-    }
-}
-class Person{
-    private String fName;
-    private String lName;
-
-    public Person(String fName,String lName){
-        this.fName=fName;
-        this.lName=lName;
-    }
-
-    public String getfName(){
-        return fName;
-    }
-    public String getlName(){
-        return lName;
-    }
-}
-class Employee extends Person{
-    private int eId;
-    private String job;
-
-    public Employee(String fName,String lName,int eId,String job){
-        super(fName,lName);
-        this.eId=eId;
-        this.job=job;
-    }
-    public int geteId(){
-        return eId;
-    }
-
-    public String getLastName() {
-        return super.getlName() + ", " + job;
-    }
-}
+//public class Q7 {
+//    public static void main(String[] args) {
+//
+//        Employee employee1 = new Employee("Kortney", "Rosalee", 4451, "HR Manager");
+//        System.out.println(employee1.getfName() + " " + employee1.getLastName() + " (" + employee1.geteId() + ")");
+//
+//    }
+//}
+//class Person{
+//    private String fName;
+//    private String lName;
+//
+//    public Person(String fName,String lName){
+//        this.fName=fName;
+//        this.lName=lName;
+//    }
+//
+//    public String getfName(){
+//        return fName;
+//    }
+//    public String getlName(){
+//        return lName;
+//    }
+//}
+//class Employee extends Person{
+//    private int eId;
+//    private String job;
+//
+//    public Employee(String fName,String lName,int eId,String job){
+//        super(fName,lName);
+//        this.eId=eId;
+//        this.job=job;
+//    }
+//    public int geteId(){
+//        return eId;
+//    }
+//
+//    public String getLastName() {
+//        return super.getlName() + ", " + job;
+//    }
+//}
