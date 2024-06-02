43cfc4eec468927e39991091dcff6a68553f33b7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 21:30:52 +0530

    first commit

--- Full Java Practice/src/Interface/Q1.java
+++ Full Java Practice/src/Interface/Q1.java
@@ -4,18 +4,42 @@ package Interface;
  * Write a Java program to create an interface Shape with the getArea() method. Create three classes Rectangle, Circle,
  * and Triangle that implement the Shape interface. Implement the getArea() method for each of the three classes.
  */
+public class Q1{
+    public static void main(String[] args) {
+        Rectangle r=new Rectangle(4,8);
+        Circle c=new Circle(7);
 
-// This is an interface named 'Shape' that defines a contract for classes to implement.
-public interface Shape{
-    // Abstract method signature for getting the area of a shape.
-    double getArea();
+        // Print the area of the Rectangle
+        System.out.println("Area of the Rectangle: " + r.getArea());
+
+        // Print the area of the Circle
+        System.out.println("Area of the Circle: " + c.getArea());
+
+    }
 }
+
 class Rectangle implements Shape{
     private double length;
-    private double height;
+    private double width;
 
-    public Rectangle(double length,double height){
+    public Rectangle(double length,double width){
         this.length=length;
-        this.height=height;
+        this.width=width;
+    }
+    @Override
+    public double getArea(){
+        return length*width;
+    }
+}
+class Circle implements Shape{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius=radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI*radius*radius;
     }
 }
---
+++ Full Java Practice/src/Interface/Shape.java
@@ -0,0 +1,7 @@
+package Interface;
+
+// This is an interface named 'Shape' that defines a contract for classes to implement.
+public interface Shape{
+    // Abstract method signature for getting the area of a shape.
+    double getArea();
+}
