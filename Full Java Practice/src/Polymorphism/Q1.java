ee02c51345f0c5c7a84ce8cb6329d05a2633a31f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 09:45:54 +0530

    first commit

--- Full Java Practice/src/Polymorphism/Q1.java
+++ Full Java Practice/src/Polymorphism/Q1.java
@@ -1,4 +1,11 @@
 package Polymorphism;
 
+/**
+ * Write a Java program to create a base class Animal (Animal Family) with a method called Sound(). Create two subclasses
+ * Bird and Cat. Override the Sound() method in each subclass to make a specific sound for each animal.
+ */
 public class Q1 {
+    public static void main(String[] args) {
+
+    }
 }
