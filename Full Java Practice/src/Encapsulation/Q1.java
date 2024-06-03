dc0ff79690ccb02ac1b801fa54e27c1b8cf2acf2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 09:35:51 +0530

    first commit

--- Full Java Practice/src/Encapsulation/Q1.java
+++ Full Java Practice/src/Encapsulation/Q1.java
@@ -1,4 +1,11 @@
 package Encapsulation;
 
+/**
+ * Write a Java program to create a class called Person with private instance variables name, age. and country. Provide
+ * public getter and setter methods to access and modify these variables.
+ */
 public class Q1 {
+    public static void main(String[] args) {
+
+    }
 }
