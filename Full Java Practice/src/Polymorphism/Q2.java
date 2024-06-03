f0ebaabb6239d0ae763ae08bb7d87a0c21c7ed1f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 10:34:23 +0530

    first commit

--- Full Java Practice/src/Polymorphism/Q2.java
+++ Full Java Practice/src/Polymorphism/Q2.java
@@ -7,5 +7,42 @@ package Polymorphism;
 public class Q2 {
     public static void main(String[] args) {
 
+        Car car =new Car();
+        Bicycle b =new Bicycle();
+        System.out.println("Car initial speed: " + car.getSpeed());
+        System.out.println("Motorcycle initial speed: " + b.getSpeed());
+
+        car.speedUp();
+        b.speedUp();
+
+        System.out.println();
+
+        System.out.println("Car initial speed: " + car.getSpeed());
+        System.out.println("Motorcycle initial speed: " + b.getSpeed());
+
+
+    }
+}
+class Vehicle{
+    private int speed;
+    public void speedUp(){
+        speed+=10;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+}
+class Car extends Vehicle{
+    @Override
+    public void speedUp(){
+        super.speedUp();
+        System.out.println("\nCar speed increased by 22 units.");
+    }
+}
+class Bicycle extends Vehicle{
+    @Override
+    public void speedUp() {
+        super.speedUp();
+        System.out.println("Motorcycle speed increased by 12 units");
     }
 }
