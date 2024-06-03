5985cf465371763e3081ffa3d66c682238712fe0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 10:17:56 +0530

    first commit

--- Full Java Practice/src/Interface/Q2.java
+++ Full Java Practice/src/Interface/Q2.java
@@ -1,2 +1,11 @@
-package Interface;public class Q2 {
+package Interface;
+
+/**
+ * Write a Java program to create a Animal interface with a method called bark() that takes no arguments and returns void.
+ * Create a Dog class that implements Animal and overrides speak() to print "Dog is barking".
+ */
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
 }
