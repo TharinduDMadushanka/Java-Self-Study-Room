29d857f71e070a9b988a2eb99553217b97b35380
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 19:25:05 +0530

    first commit

--- Full Java Practice/src/Abstract/Classes/Q5.java
+++ Full Java Practice/src/Abstract/Classes/Q5.java
@@ -20,3 +20,23 @@ abstract class Employee{
     public abstract double calcSalary();
     public abstract void displayInfo();
 }
+class Manager extends Employee{
+    private double bonus;
+    public Manager(String name,double baseSalary,double bonus){
+        super(name, baseSalary);
+        this.bonus=bonus;
+    }
+
+    @Override
+    public double calcSalary() {
+        return baseSalary+bonus;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Manager Name: " + name);
+        System.out.println("Base Salary: $" + baseSalary);
+        System.out.println("Bonus: $" + bonus);
+        System.out.println("Total Salary: $" + calcSalary());
+    }
+}
