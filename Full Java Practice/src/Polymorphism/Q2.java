9c06fe8039ae84a095ec910505b4806c7567a5e1
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 10:26:19 +0530

    first commit

---
+++ Full Java Practice/src/Polymorphism/Q2.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+/**
+ * Write a Java program to create a class Vehicle with a method called speedUp(). Create two subclasses Car and Bicycle.
+ * Override the speedUp() method in each subclass to increase the vehicle's speed differently.
+ */
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
+}
