4eaab0777809d8af253b4236b02be4c13976d21d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 19:47:20 +0530

    first commit

---
+++ Full Java Practice/src/Polymorphism/Q3.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+/**
+ * Write a Java program to create a base class Shape with a method called calculateArea(). Create three subclasses: Circle,
+ * Rectangle, and Triangle. Override the calculateArea() method in each subclass to calculate and return the shape's area.
+ */
+public class Q3 {
+    public static void main(String[] args) {
+
+    }
+}
