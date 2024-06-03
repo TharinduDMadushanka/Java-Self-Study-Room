be22b4b06dddcf251207b9b5f1bfa93b2c8789fa
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 09:52:39 +0530

    first commit

---
+++ Full Java Practice/src/Abstract/Classes/Q4.java
@@ -0,0 +1,11 @@
+package Abstract.Classes;
+
+/**
+ * Write a Java program to create an abstract class Animal with abstract methods eat() and sleep(). Create subclasses Lion,
+ * Tiger, and Deer that extend the Animal class and implement the eat() and sleep() methods differently based on their specific behavior.
+ */
+public class Q4 {
+    public static void main(String[] args) {
+
+    }
+}
