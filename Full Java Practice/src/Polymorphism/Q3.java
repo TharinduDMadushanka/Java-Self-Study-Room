83a6fb2781b3db4164b706bb183fec4ea4b38066
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 19:51:44 +0530

    first commit

--- Full Java Practice/src/Polymorphism/Q3.java
+++ Full Java Practice/src/Polymorphism/Q3.java
@@ -7,5 +7,38 @@ package Polymorphism;
 public class Q3 {
     public static void main(String[] args) {
 
+        Circle c=new Circle(7);
+        c.calcArea();
+        System.out.println();
+
+        Rectangle r =new Rectangle(5);
+        r.calcArea();
+
+    }
+}
+class Base{
+    public double calcArea(){
+        return 0;
+    }
+}
+class Circle extends Base{
+    private int radius;
+    public Circle(int radius){
+        this.radius=radius;
+    }
+    @Override
+    public double calcArea(){
+        return 2*Math.PI*radius;
+    }
+}
+class Rectangle extends Base{
+    private int side;
+
+    public Rectangle(int side) {
+        this.side = side;
+    }
+    public double calcArea(){
+
+        return side*4;
     }
 }
