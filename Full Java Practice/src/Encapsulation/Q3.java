4a31e9b0a50ebc669440ee83746d2ea80db82abf
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 19:43:11 +0530

    first commit

--- Full Java Practice/src/Encapsulation/Q3.java
+++ Full Java Practice/src/Encapsulation/Q3.java
@@ -1,2 +1,11 @@
-package Encapsulation;public class Q3 {
+package Encapsulation;
+
+/**
+ * Write a Java program to create a class called Rectangle with private instance variables length and width. Provide public
+ * getter and setter methods to access and modify these variables.
+ */
+public class Q3 {
+    public static void main(String[] args) {
+
+    }
 }
