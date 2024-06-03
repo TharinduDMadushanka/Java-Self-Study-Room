89d617f3037b11bae4bfb3ebf7c1de36660a9025
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-03 19:31:53 +0530

    first commit

---
+++ Full Java Practice/src/Interface/Q3.java
@@ -0,0 +1,11 @@
+package Interface;
+
+/**
+ * Write a Java program to create an interface Flyable with a method called fly_obj(). Create three classes Spacecraft,
+ * Airplane, and Helicopter that implement the Flyable interface. Implement the fly_obj() method for each of the three classes.
+ */
+public class Q3 {
+    public static void main(String[] args) {
+
+    }
+}
