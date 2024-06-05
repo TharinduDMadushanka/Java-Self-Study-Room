2b717d04039f75652595030af8608d91bd860971
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:34:41 +0530

    first commit

---
+++ Full Java Practice/src/Abstract/Classes/Q6.java
@@ -0,0 +1,11 @@
+package Abstract.Classes;
+
+/**
+ * Write a Java program to create an abstract class Shape3D with abstract methods calculateVolume() and calculateSurfaceArea(). Create subclasses
+ * Sphere and Cube that extend the Shape3D class and implement the respective methods to calculate the volume and surface area of each shape
+ */
+public class Q6 {
+    public static void main(String[] args) {
+
+    }
+}
