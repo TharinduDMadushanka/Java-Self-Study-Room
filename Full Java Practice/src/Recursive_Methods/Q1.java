adb083565b874bb5be2d2c1fe05a5a65d0fa3a79
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:20:27 +0530

    first commit

--- Full Java Practice/src/Recursive_Methods/Q1.java
+++ Full Java Practice/src/Recursive_Methods/Q1.java
@@ -1,10 +1,19 @@
 package Recursive_Methods;
 
+import java.util.Scanner;
+
 /**
  * Write a Java recursive method to calculate the factorial of a given positive integer
  */
 public class Q1 {
     public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
 
+        System.out.print("Enter a nb: ");
+        int nb = input.nextInt();
+
+        for (int i=nb;i<0;i--){
+            System.out.print(nb+" x");
+        }
     }
 }
