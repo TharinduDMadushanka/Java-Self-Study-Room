4a811e7c89166781b5822f6e7eeb342b35b4469b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:18:56 +0530

    first commit

---
+++ Full Java Practice/src/Recursive_Methods/Q1.java
@@ -0,0 +1,10 @@
+package Recursive_Methods;
+
+/**
+ * Write a Java recursive method to calculate the factorial of a given positive integer
+ */
+public class Q1 {
+    public static void main(String[] args) {
+
+    }
+}
