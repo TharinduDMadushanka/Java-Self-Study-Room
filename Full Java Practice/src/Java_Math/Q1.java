46c21fe95eb189813e1915ac3ffed8f557c296c3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:32:40 +0530

    first commit

--- Full Java Practice/src/Java_Math/Q1.java
+++ Full Java Practice/src/Java_Math/Q1.java
@@ -1,10 +1,16 @@
 package Java_Math;
 
+import java.util.Scanner;
+
 /**
  * Write a Java program to round up integer division results.
  */
 public class Q1 {
     public static void main(String[] args) {
 
+        int tot_theory_marks = 350, tot_practical_marks = 90, tot_marks = 500;
+        int percentage_of_marks = ((tot_theory_marks + tot_practical_marks) * 100) / tot_marks;
+        System.out.print("\nPercentage of Marks: " + percentage_of_marks + "%\n");
     }
 }
+
