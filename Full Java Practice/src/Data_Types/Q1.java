e705c12d36301211db71425bec0d3a24a80ec1d1
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:13:17 +0530

    first commit

---
+++ Full Java Practice/src/Data_Types/Q1.java
@@ -0,0 +1,10 @@
+package Data_Types;
+
+/**
+ * Write a Java program to convert temperature from Fahrenheit to Celsius degrees.
+ */
+public class Q1 {
+    public static void main(String[] args) {
+
+    }
+}
