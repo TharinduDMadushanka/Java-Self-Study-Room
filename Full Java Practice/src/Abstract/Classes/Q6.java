71233640e5d41ff572d44b46d789e22fd93a220c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 09:16:58 +0530

    first commit

--- Full Java Practice/src/Abstract/Classes/Q6.java
+++ Full Java Practice/src/Abstract/Classes/Q6.java
@@ -9,3 +9,28 @@ public class Q6 {
 
     }
 }
+
+abstract class Shape3D {
+    public abstract double calculateVolume();
+
+    public abstract double calculateSurfaceArea();
+}
+
+class Sphere extends Shape3D {
+
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public double calculateSurfaceArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+}
