b77723e59d09361c5df0a85ea71c94cac57a92d7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:08:06 +0530

    first commit

--- Full Java Practice/src/Methods/Q1.java
+++ Full Java Practice/src/Methods/Q1.java
@@ -1,10 +1,22 @@
 package Methods;
 
+import java.util.Scanner;
+
 /**
  *  Write a Java method to find the smallest number among three numbers.
  */
 public class Q1 {
     public static void main(String[] args) {
 
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter nb1: ");
+        int nb1= input.nextInt();
+
+        System.out.print("Enter nb2: ");
+        int nb2= input.nextInt();
+
+        System.out.print("Enter nb3: ");
+        int nb3= input.nextInt();
     }
 }
