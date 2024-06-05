ecb6b0469092dfdf5490bc1cea0d0641bed885d4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 09:20:17 +0530

    first commit

--- Full Java Practice/src/Abstract/Classes/Q6.java
+++ Full Java Practice/src/Abstract/Classes/Q6.java
@@ -7,6 +7,14 @@ package Abstract.Classes;
 public class Q6 {
     public static void main(String[] args) {
 
+        Shape3D sphere =new Sphere(7);
+        Shape3D cube =new Cube(6);
+
+        System.out.println("Sphere Volume: " + sphere.calculateVolume());
+        System.out.println("Sphere Surface Area: " + sphere.calculateSurfaceArea());
+
+        System.out.println("Cube Volume: " + cube.calculateVolume());
+        System.out.println("Cube Surface Area: " + cube.calculateSurfaceArea());
     }
 }
 
