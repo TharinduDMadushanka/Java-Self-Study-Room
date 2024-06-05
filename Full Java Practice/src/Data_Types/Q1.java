ceae69b9dd9fdd7ed47cc6d66674525311c82971
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-05 08:15:34 +0530

    first commit

--- Full Java Practice/src/Data_Types/Q1.java
+++ Full Java Practice/src/Data_Types/Q1.java
@@ -1,10 +1,20 @@
 package Data_Types;
 
+import java.util.Scanner;
+
 /**
  * Write a Java program to convert temperature from Fahrenheit to Celsius degrees.
  */
 public class Q1 {
     public static void main(String[] args) {
 
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter temperature in F:");
+        double temp= input.nextDouble();
+
+        double result = (temp-32)*5/9;
+
+        System.out.println("Celsius: " +result);
     }
 }
