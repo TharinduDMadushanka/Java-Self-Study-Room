f8891e385f1dac123b8792cddce4a160308bd6e8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:38:36 +0530

    first commit

--- Full Java Practice/src/Conditional_Statement/Q2.java
+++ Full Java Practice/src/Conditional_Statement/Q2.java
@@ -19,5 +19,18 @@ public class Q2 {
         System.out.print("Input c: ");
         int c = input.nextInt();
 
+        int result = b*b -4*a*c;
+
+        if (result > 0) {
+            double r1 =(-b +Math.sqrt(result))/(2*a);
+            double r2 =(-b -Math.sqrt(result))/(2*a);
+            System.out.println("The root are "+r1+" and "+r2);
+        } else if (result==0) {
+            double r1 = (double) -b /(2*a);
+            System.out.println("The root are "+r1);
+        }else {
+            System.out.println("The equation has no root.");
+        }
+
     }
 }
