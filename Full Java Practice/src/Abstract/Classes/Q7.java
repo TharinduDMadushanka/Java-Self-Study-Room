b346154c40ccf024c97a4371ebfe04ecab8e5ad9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:46:00 +0530

    first commit

---
+++ Full Java Practice/src/Abstract/Classes/Q7.java
@@ -0,0 +1,11 @@
+package Abstract.Classes;
+
+/**
+ * Write a Java program to create an abstract class Vehicle with abstract methods startEngine() and stopEngine(). Create subclasses
+ * Car and  Motorcycle that extend the Vehicle class and implement the respective methods to start and stop the engines for each vehicle type.
+ */
+public class Q7 {
+    public static void main(String[] args) {
+
+    }
+}
