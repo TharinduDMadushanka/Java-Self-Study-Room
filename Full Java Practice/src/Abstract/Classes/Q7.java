2a272acfb45ab6614e6d7f47dc54a516526880ec
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:47:36 +0530

    first commit

--- Full Java Practice/src/Abstract/Classes/Q7.java
+++ Full Java Practice/src/Abstract/Classes/Q7.java
@@ -17,4 +17,7 @@ class Car extends Vehicle {
     public void startEngine() {
         System.out.println("Car Engine Started");
     }
+    public void stopEngine() {
+        System.out.println("Car Engine Stopped");
+    }
 }
