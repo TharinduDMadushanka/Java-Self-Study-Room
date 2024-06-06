81f3c9bb3a362cd7478299347c7d6b0cfcd06788
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:50:34 +0530

    first commit

---
+++ Full Java Practice/src/Interface/Q4.java
@@ -0,0 +1,13 @@
+package Interface;
+
+/**
+ * Write a Java programming to create a banking system with three classes - Bank, Account, SavingsAccount, and CurrentAccount.
+ * The bank should have a list of accounts and methods for adding them. Accounts should be an interface with methods to deposit,
+ * withdraw, calculate interest, and view balances. SavingsAccount and CurrentAccount should implement the Account interface and
+ * have their own unique methods.
+ */
+public class Q4 {
+    public static void main(String[] args) {
+
+    }
+}
