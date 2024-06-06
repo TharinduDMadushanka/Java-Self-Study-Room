d58892433fb88544ece2bb7cf77a4bbf718c757e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:39:37 +0530

    first commit

---
+++ Full Java Practice/src/Recursive_Methods/Q2.java
@@ -0,0 +1,10 @@
+package Recursive_Methods;
+
+/**
+ * Write a Java recursive method to calculate the sum of all numbers from 1 to n.
+ */
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
+}
