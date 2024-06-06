ed5d3bbd2544549e9cfffea88dbeab79015e9ffd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:34:48 +0530

    first commit

--- Full Java Practice/src/Conditional_Statement/Q2.java
+++ Full Java Practice/src/Conditional_Statement/Q2.java
@@ -1,10 +1,23 @@
 package Conditional_Statement;
 
+import java.util.Scanner;
+
 /**
  * Write a Java method to compute the average of three numbers.
  */
 public class Q2 {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a: ");
+        int a = input.nextInt();
+
+        System.out.print("Input b: ");
+        int b = input.nextInt();
+
+        System.out.print("Input c: ");
+        int c = input.nextInt();
 
     }
 }
