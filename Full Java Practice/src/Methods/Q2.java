f8ceb26a23cba9a8738ed0a3643e35d8e83580b3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:31:33 +0530

    first commit

--- Full Java Practice/src/Methods/Q2.java
+++ Full Java Practice/src/Methods/Q2.java
@@ -1,10 +1,30 @@
 package Methods;
 
+import java.util.Scanner;
+
 /**
  * Write a Java method to compute the average of three numbers.
  */
 public class Q2 {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first nb:");
+        int nb1 = input.nextInt();
+
+        System.out.println("Enter your second nb:");
+        int nb2 = input.nextInt();
+
+        System.out.println("Enter your third nb:");
+        int nb3 = input.nextInt();
+
+    }
+    public void calcAverage(int nb1, int nb2, int nb3){
+
+        int sum=nb1+nb2+nb3;
+        int avg=sum/3;
+
+        System.out.println("The average is "+avg);
     }
 }
