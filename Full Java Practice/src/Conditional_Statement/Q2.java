9e5a4d98106a5f13f9129c83ba851521f6468965
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-06 08:33:56 +0530

    first commit

---
+++ Full Java Practice/src/Conditional_Statement/Q2.java
@@ -0,0 +1,10 @@
+package Conditional_Statement;
+
+/**
+ * Write a Java method to compute the average of three numbers.
+ */
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
+}
