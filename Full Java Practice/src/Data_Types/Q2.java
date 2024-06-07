b24518cf897fc61d6be19deccc18266adbd424f3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 12:13:31 +0530

    first commit

--- Full Java Practice/src/Data_Types/Q2.java
+++ Full Java Practice/src/Data_Types/Q2.java
@@ -1,10 +1,20 @@
 package Data_Types;
 
+import java.util.Scanner;
+
 /**
  * Write a Java program that reads a number in inches and converts it to meters.
  */
 public class Q2 {
     public static void main(String[] args) {
 
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter your measurement in inch:");
+        double inch = input.nextDouble();
+
+        double meter = inch / 2.54;
+
+        System.out.println("Your measurement in inch is " + meter);
     }
 }
