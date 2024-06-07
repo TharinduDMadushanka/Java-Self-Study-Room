5ef4eab2b86b7c87d4f8ad4f336ebf0c0fab4156
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:25:32 +0530

    first commit

---
+++ Full Java Practice/src/Polymorphism/Q4.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+/**
+ * Write a Java program to create a class Employee with a method called calculateSalary(). Create two subclasses Manager
+ * and Programmer. In each subclass, override the calculateSalary() method to calculate and return the salary based on their specific roles.
+ */
+public class Q4 {
+    public static void main(String[] args) {
+
+    }
+}
