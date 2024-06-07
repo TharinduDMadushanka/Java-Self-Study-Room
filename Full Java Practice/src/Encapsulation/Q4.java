c90f035407c847d96fd5978fef64d7b0e01bfc93
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:22:23 +0530

    first commit

--- Full Java Practice/src/Encapsulation/Q4.java
+++ Full Java Practice/src/Encapsulation/Q4.java
@@ -1,5 +1,7 @@
 package Encapsulation;
 
+import java.util.Scanner;
+
 /**
  * Write a Java program to create a class called Employee with private instance variables employee_id, employee_name, and
  * employee_salary. Provide public getter and setter methods to access and modify the id and name variables, but provide a
@@ -8,6 +10,16 @@ package Encapsulation;
 public class Q4 {
     public static void main(String[] args) {
 
+        Scanner input =new Scanner(System.in);
+
+        System.out.println("Enter you id: ");
+        int id =input.nextInt();
+
+        System.out.println("Enter your name: ");
+        String name=input.next();
+
+        System.out.println("Enter your salary: ");
+        double salary=input.nextDouble();
     }
 }
 class Employee{
