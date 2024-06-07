4d41b8c308272a3cd865cd3b056b87358aa15686
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:30:01 +0530

    first commit

--- Full Java Practice/src/Polymorphism/Q4.java
+++ Full Java Practice/src/Polymorphism/Q4.java
@@ -34,3 +34,17 @@ class Employee{
         return 0;
     }
 }
+class Manager extends Employee{
+    private double salary;
+    private double bonus;
+
+    public Manager(String name,double salary, double bonus) {
+        super(name,"Manager");
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+    public double getSalary(){
+        return salary+bonus;
+    }
+
+}
