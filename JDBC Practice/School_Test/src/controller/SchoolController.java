27e073b07436207b2113bacf167ebca99d13b1c7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 19:56:43 +0530

    first commit

--- JDBC Practice/School_Test/src/controller/SchoolController.java
+++ JDBC Practice/School_Test/src/controller/SchoolController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import dto.SchoolDto;
+import model.SchoolModel;
+
+import java.sql.SQLException;
+
 public class SchoolController {
+    private SchoolModel schoolModel;
+
+    public SchoolController() throws SQLException, ClassNotFoundException {
+        this.schoolModel=new SchoolModel();
+    }
+    public String saveSchool(SchoolDto schoolDto) throws SQLException, ClassNotFoundException {
+        String resp=schoolModel.saveSchool(schoolDto);
+        return resp;
+    }
 }
