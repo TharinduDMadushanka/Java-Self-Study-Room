6983e5fd9a8332ac5943f5ed15e41753058942cd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:19:04 +0530

    first commit

---
+++ Full Java Practice/src/Encapsulation/Q4.java
@@ -0,0 +1,12 @@
+package Encapsulation;
+
+/**
+ * Write a Java program to create a class called Employee with private instance variables employee_id, employee_name, and
+ * employee_salary. Provide public getter and setter methods to access and modify the id and name variables, but provide a
+ * getter method for the salary variable that returns a formatted string.
+ */
+public class Q4 {
+    public static void main(String[] args) {
+
+    }
+}
