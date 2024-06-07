dfd71ebae7d86daa639a90b8330b744df29b6492
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:16:09 +0530

    first commit

--- Full Java Practice/src/Interface/Q5.java
+++ Full Java Practice/src/Interface/Q5.java
@@ -13,3 +13,22 @@ interface Resizable {
     void resizeWidth(int width);
     void resizeHeight(int height);
 }
+class Rectangle implements Resizable {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    public void resizeWidth(int width) {
+        this.width = width;
+    }
+    public void resizeHeight(int height) {
+        this.height = height;
+    }
+    public void printSize(){
+        System.out.println(width + "x" + height);
+    }
+}
