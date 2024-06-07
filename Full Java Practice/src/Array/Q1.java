523714ff34c1069fd27c6b9fc54c256891d7b6d0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:57:52 +0530

    first commit

--- Full Java Practice/src/Array/Q1.java
+++ Full Java Practice/src/Array/Q1.java
@@ -7,5 +7,22 @@ public class Q1 {
     public static void main(String[] args) {
 
         int[] arr1 = {11, 23, 34, 23, 5};
+
+        for (int i = 0; i < arr1.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr1.length; j++) {
+                if (arr1[j] < arr1[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr1[min];
+            arr1[min] = arr1[i];
+            arr1[i] = temp;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]+", ");
+        }
+        System.out.println();
     }
 }
