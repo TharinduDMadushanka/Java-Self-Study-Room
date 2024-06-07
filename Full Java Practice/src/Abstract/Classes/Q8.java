65ce535abe4cda12e05b0f7acaf4d2fad14994e4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:08:05 +0530

    first commit

---
+++ Full Java Practice/src/Abstract/Classes/Q8.java
@@ -0,0 +1,11 @@
+package Abstract.Classes;
+
+/**
+ * Write a Java program to create an abstract class Person with abstract methods eat() and exercise(). Create subclasses
+ * Athlete and LazyPerson that extend the Person class and implement the respective methods to describe how each person eats and exercises.
+ */
+public class Q8 {
+    public static void main(String[] args) {
+
+    }
+}
