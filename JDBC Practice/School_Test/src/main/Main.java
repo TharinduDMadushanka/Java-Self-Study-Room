8f605fa82e5b9d36cdd25f82f516c8e3fe08eeab
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 20:04:10 +0530

    first commit

--- JDBC Practice/School_Test/src/main/Main.java
+++ JDBC Practice/School_Test/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import view.SchoolView;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         new SchoolView().setVisible(true);
     }
