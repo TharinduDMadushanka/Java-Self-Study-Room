ae7101c4ec0302b506eac150f9579bbf549db185
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:13:03 +0530

    first commit

---
+++ Full Java Practice/src/Interface/Q5.java
@@ -0,0 +1,11 @@
+package Interface;
+
+/**
+ * Write a Java program to create an interface Resizable with methods resizeWidth(int width) and resizeHeight(int height)
+ * that allow an object to be resized. Create a class Rectangle that implements the Resizable interface and implements the resize methods.
+ */
+public class Q5 {
+    public static void main(String[] args) {
+
+    }
+}
