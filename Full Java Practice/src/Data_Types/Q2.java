7517d442737cecc0d899d14826520674eeccc9ac
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 12:14:59 +0530

    first commit

--- Full Java Practice/src/Data_Types/Q2.java
+++ Full Java Practice/src/Data_Types/Q2.java
@@ -13,7 +13,7 @@ public class Q2 {
         System.out.print("Enter your measurement in inch:");
         double inch = input.nextDouble();
 
-        double meter = inch / 0.0254;
+        double meter = inch / 39.37;
 
         System.out.println("Your measurement in inch is " + meter);
     }
