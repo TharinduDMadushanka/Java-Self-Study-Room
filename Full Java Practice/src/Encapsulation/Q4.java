8229ecb435cdfa96e44701998b5f3f07b6ce103b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:21:18 +0530

    first commit

--- Full Java Practice/src/Encapsulation/Q4.java
+++ Full Java Practice/src/Encapsulation/Q4.java
@@ -10,3 +10,27 @@ public class Q4 {
 
     }
 }
+class Employee{
+    private int id;
+    private String name;
+    private double salary;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
