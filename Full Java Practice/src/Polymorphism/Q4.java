e9930493cc3e41a91c2fade482817ea6e50a6d9a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-07 21:27:45 +0530

    first commit

--- Full Java Practice/src/Polymorphism/Q4.java
+++ Full Java Practice/src/Polymorphism/Q4.java
@@ -9,3 +9,28 @@ public class Q4 {
 
     }
 }
+class Employee{
+    private String name;
+    private String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role=role;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+        this.role=role;
+    }
+    public double calcSalary(){
+        return 0;
+    }
+}
