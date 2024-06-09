3543e44e82c0e0b86b3cd478bf145a1cc395493a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-09 08:06:34 +0530

    complete item crud

---
+++ Full Java Practice/src/Data_Types/Q3.java
@@ -0,0 +1,15 @@
+package Data_Types;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program that reads an integer between 0 and 1000 and adds all the digits in the integer.
+ */
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter an integer between 0 - 1000: ");
+        int nb =input.nextInt();
+    }
+}
