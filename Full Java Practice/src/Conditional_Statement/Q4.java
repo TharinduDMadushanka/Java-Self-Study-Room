b34790f89c1f2ac7831906b6114f30f874d82fa8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-09 08:17:13 +0530

    complete item crud

---
+++ Full Java Practice/src/Conditional_Statement/Q4.java
@@ -0,0 +1,11 @@
+package Conditional_Statement;
+
+/**
+ * Write a Java program that reads a floating-point number and prints "zero" if the number is zero. Otherwise, print
+ * "positive" or "negative". Add "small" if the absolute value of the number is less than 1, or "large" if it exceeds 1,000,000.
+ */
+public class Q4 {
+    public static void main(String[] args) {
+
+    }
+}
