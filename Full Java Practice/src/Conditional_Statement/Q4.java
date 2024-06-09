cedb9f4cd7bbf2c0964aed5aca92091b6a717652
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-09 08:21:32 +0530

    complete item crud

--- Full Java Practice/src/Conditional_Statement/Q4.java
+++ Full Java Practice/src/Conditional_Statement/Q4.java
@@ -1,5 +1,7 @@
 package Conditional_Statement;
 
+import java.util.Scanner;
+
 /**
  * Write a Java program that reads a floating-point number and prints "zero" if the number is zero. Otherwise, print
  * "positive" or "negative". Add "small" if the absolute value of the number is less than 1, or "large" if it exceeds 1,000,000.
@@ -7,5 +9,43 @@ package Conditional_Statement;
 public class Q4 {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input value: ");
+        double input = in.nextDouble();
+
+        if (input > 0)
+        {
+            if (input < 1)
+            {
+                System.out.println("Positive small number");
+            }
+            else if (input > 1000000)
+            {
+                System.out.println("Positive large number");
+            }
+            else
+            {
+                System.out.println("Positive number");
+            }
+        }
+        else if (input < 0)
+        {
+            if (Math.abs(input) < 1)
+            {
+                System.out.println("Negative small number");
+            }
+            else if (Math.abs(input) > 1000000)
+            {
+                System.out.println("Negative large number");
+            }
+            else
+            {
+                System.out.println("Negative number");
+            }
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
     }
 }
