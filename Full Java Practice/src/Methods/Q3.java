bc27b9aa5ef67d06e9e7d8815e09b3ba72485281
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-09 08:04:23 +0530

    complete item crud

--- Full Java Practice/src/Methods/Q3.java
+++ Full Java Practice/src/Methods/Q3.java
@@ -22,14 +22,15 @@ public class Q3 {
         if (chars.length%2==0){
             for (int i = 0; i < chars.length/2; i+=2){
                 i=chars.length/2-1;
-                System.out.print("Middle characters :");
+                System.out.print("Middle characters are :");
                 System.out.print(chars[i]);
                 System.out.print(", "+chars[i+1]);
             }
         }else {
             for (int i = 0; i < chars.length/2; i++) {
                 i=chars.length/2;
-                System.out.println(chars[i]);
+                System.out.print("Middle character is: ");
+                System.out.print(chars[i]);
             }
         }
     }
