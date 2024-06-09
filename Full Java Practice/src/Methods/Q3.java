0c5c3dd0db671678f3710a6f02cc93f84f83e3b2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-09 07:58:30 +0530

    complete item crud

--- Full Java Practice/src/Methods/Q3.java
+++ Full Java Practice/src/Methods/Q3.java
@@ -15,8 +15,13 @@ public class Q3 {
 
         char[] chars = word.toCharArray();
 
-        for (int i = 0; i < chars.length; i++) {
-            System.out.print(chars[i]+", ");
+//        for (int i = 0; i < chars.length; i++) {
+//            System.out.print(chars[i]+", ");
+//        }
+
+        for (int i = 0; i < chars.length/2; i++) {
+            i=chars.length/2;
+            System.out.println(chars[i]);
         }
 
     }
