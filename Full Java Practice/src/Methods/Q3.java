2c93da0ddf22af3f01452e567b9122eb341324e5
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-09 07:54:14 +0530

    complete item crud

--- Full Java Practice/src/Methods/Q3.java
+++ Full Java Practice/src/Methods/Q3.java
@@ -1,10 +1,19 @@
 package Methods;
 
+import java.util.Scanner;
+
 /**
  * Write a Java method to display the middle character of a string.
  */
 public class Q3 {
     public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String word = input.nextLine();
+
+
 
     }
 }
