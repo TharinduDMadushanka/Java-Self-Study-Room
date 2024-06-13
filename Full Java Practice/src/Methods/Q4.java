6a1d4387cce8193a84e719fb27b5c8a562371596
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-13 07:38:57 +0530

    first commit

--- Full Java Practice/src/Methods/Q4.java
+++ Full Java Practice/src/Methods/Q4.java
@@ -1,10 +1,22 @@
 package Methods;
 
+import java.util.Scanner;
+
 /**
  * Write a Java method to count all vowels in a string.
  */
 public class Q4 {
     public static void main(String[] args) {
+
+
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = input.nextLine();
+
+        char[] arr = word.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {}
 
     }
 }
