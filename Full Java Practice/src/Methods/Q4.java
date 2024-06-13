746a7a4e98a898ca7492e46f17be176d157e46ee
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-13 07:42:27 +0530

    first commit

--- Full Java Practice/src/Methods/Q4.java
+++ Full Java Practice/src/Methods/Q4.java
@@ -18,10 +18,17 @@ public class Q4 {
     }
     public static void calcVowels(String word) {
 
+        int count=0;
         char[] arr = word.toCharArray();
+        char[] vowel={'a','e','i','o','u'};
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            for (int j = 0; j < vowel.length; j++) {
+                if (arr[i] == vowel[j]) {
+                    count++;
+                }
+            }
         }
+        System.out.println("Vowels: " + count)  ;
     }
 }
