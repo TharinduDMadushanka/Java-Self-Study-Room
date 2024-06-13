5c6d2e932cbf439e883d7b3df9f383c140b9153a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-13 07:48:22 +0530

    first commit

---
+++ Full Java Practice/src/Data_Types/Q4.java
@@ -0,0 +1,17 @@
+package Data_Types;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program to convert minutes into years and days.
+ */
+public class Q4 {
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+        System.out.print("Enter the number of minutes: ");
+        double minutes = input.nextDouble();
+
+    }
+}
