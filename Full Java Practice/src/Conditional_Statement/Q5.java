d1d9b3be164989bd33c4c2454ff173c6c5263860
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-13 07:51:16 +0530

    first commit

---
+++ Full Java Practice/src/Conditional_Statement/Q5.java
@@ -0,0 +1,16 @@
+package Conditional_Statement;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program that takes a number from the user and generates an integer between 1 and 7. It displays the weekday name.
+ */
+public class Q5 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number between 1 and 7");
+        int nb = input.nextInt();
+    }
+}
