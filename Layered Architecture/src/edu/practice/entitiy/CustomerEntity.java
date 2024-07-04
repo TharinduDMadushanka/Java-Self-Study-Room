0bf0a9a81d5fb8c0fb1646f9a303bd8a90222eae
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-04 20:20:10 +0530

    create CustomerEntity class

---
+++ Layered Architecture/src/edu/practice/entitiy/CustomerEntity.java
@@ -0,0 +1,115 @@
+package edu.practice.entitiy;
+
+public class CustomerEntity {
+    private String id;
+    private String title;
+    private String name;
+    private String dob;
+    private double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postal;
+
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(String id, String title, String name, String dob, double salary, String address, String city, String province, String postal) {
+        this.id = id;
+        this.title = title;
+        this.name = name;
+        this.dob = dob;
+        this.salary = salary;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postal = postal;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerEntity{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", name='" + name + '\'' +
+                ", dob='" + dob + '\'' +
+                ", salary=" + salary +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", postal='" + postal + '\'' +
+                '}';
+    }
+}
