753f7f3b0ea5bbff78a17993349b0a46302ffe69
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-04 14:33:28 +0530

    complete ItemController class

--- Layered Architecture/src/edu/practice/controller/ItemController.java
+++ Layered Architecture/src/edu/practice/controller/ItemController.java
@@ -1,7 +1,27 @@
 package edu.practice.controller;
 
+import edu.practice.dto.ItemDto;
+import edu.practice.service.ServiceFactory;
 import edu.practice.service.custom.ItemService;
 
+import java.util.ArrayList;
+
 public class ItemController {
-    //private ItemService itemService
+    private ItemService itemService = (ItemService) ServiceFactory.getInstance().getSuperService(ServiceFactory.serviceType.ITEM);
+
+    public String saveItem(ItemDto itemDto)throws Exception{
+        return itemService.saveItem(itemDto);
+    }
+    public String updateItem(ItemDto itemDto)throws Exception{
+        return itemService.updateItem(itemDto);
+    }
+    public String deleteItem(String itemCode)throws Exception{
+        return itemService.deleteItem(itemCode);
+    }
+    public ItemDto getItem(String itemCode)throws Exception{
+        return itemService.getItem(itemCode);
+    }
+    public ArrayList<ItemDto> getAllItems()throws Exception{
+        return itemService.getAllItems();
+    }
 }
