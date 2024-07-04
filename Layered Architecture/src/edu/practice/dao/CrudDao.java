217cf4409c09d09e6364dba6ff2fde9ac65d75e1
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-04 14:07:10 +0530

    complete ItemDaoImpl class

--- Layered Architecture/src/edu/practice/dao/CrudDao.java
+++ Layered Architecture/src/edu/practice/dao/CrudDao.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface CrudDao<T, ID> extends SuperDao{
     boolean create(T t)throws Exception;
     boolean update(T t)throws Exception;
-    boolean delete(T t)throws Exception;
+    boolean delete(ID id)throws Exception;
     T get(ID id)throws Exception;
     ArrayList<T> getAll()throws Exception;
 }
--- Layered Architecture/src/edu/practice/dao/custom/impl/ItemDaoImpl.java
+++ Layered Architecture/src/edu/practice/dao/custom/impl/ItemDaoImpl.java
@@ -1,33 +1,55 @@
 package edu.practice.dao.custom.impl;
 
+import edu.practice.dao.CrudUtil;
 import edu.practice.dao.custom.ItemDao;
 import edu.practice.entitiy.ItemEntity;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class ItemDaoImpl implements ItemDao {
     @Override
-    public boolean create(ItemEntity itemEntity) throws Exception {
-        return false;
+    public boolean create(ItemEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO item VALUES(?,?,?,?,?)",t.getItemCode(),t.getDescription(),t.getPackSize(),
+                t.getUnitPrice(),t.getQoh());
     }
 
     @Override
-    public boolean update(ItemEntity itemEntity) throws Exception {
-        return false;
+    public boolean update(ItemEntity t) throws Exception {
+        return CrudUtil.executeUpdate("UPDATE ITEM SET Description = ?, PackSize = ?, UnitPrice = ?, QtyOnHand = ? WHERE ItemCode = ?",
+                t.getDescription(),t.getPackSize(),t.getUnitPrice(),t.getQoh(),t.getItemCode());
     }
 
     @Override
-    public boolean delete(ItemEntity itemEntity) throws Exception {
-        return false;
+    public boolean delete(String id) throws Exception {
+        return CrudUtil.executeUpdate("DELETE FROM item WHERE ItemCode = ?",id);
     }
 
     @Override
-    public ItemEntity get(String s) throws Exception {
+    public ItemEntity get(String id) throws Exception {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM item WHERE ItemCode = ?",id);
+        if(rst.next()){
+            ItemEntity entity = new ItemEntity(rst.getString("ItemCode"),
+                    rst.getString("Description"), rst.getString("PackSize"),
+                    rst.getDouble("UnitPrice"),rst.getInt("QtyOnHand"));
+
+            return entity;
+        }
         return null;
     }
 
     @Override
     public ArrayList<ItemEntity> getAll() throws Exception {
-        return null;
+        ArrayList<ItemEntity> itemEntities = new ArrayList<>();
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM Item");
+
+        while(rst.next()){
+            ItemEntity entity = new ItemEntity(rst.getString("ItemCode"),
+                    rst.getString("Description"), rst.getString("PackSize"),
+                    rst.getDouble("UnitPrice"),rst.getInt("QtyOnHand"));
+
+            itemEntities.add(entity);
+        }
+        return itemEntities;
     }
 }
