ed6600fe503ac920f158c1ea9421d9f0de9a308f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-04 11:14:41 +0530

    add ItemView UI

--- Layered Architecture/src/edu/practice/Main.java
+++ Layered Architecture/src/edu/practice/Main.java
@@ -1,7 +1,9 @@
 package edu.practice;
 
+import edu.practice.view.ItemView;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        new ItemView().setVisible(true);
     }
 }
