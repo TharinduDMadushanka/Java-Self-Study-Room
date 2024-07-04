5c1e08a3bdfcf9edc9a8b88cf0007d5dd506b7d1
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-04 11:25:11 +0530

    complete ItemDto class

---
+++ Layered Architecture/src/edu/practice/dto/ItemDto.java
@@ -0,0 +1,72 @@
+package edu.practice.dto;
+
+public class ItemDto {
+
+    private String itemCode;
+    private String description;
+    private String packSize;
+    private double unitPrice;
+    private int qoh;
+
+    public ItemDto() {
+    }
+
+    public ItemDto(String itemCode, String description, String packSize, double unitPrice, int qoh) {
+        this.itemCode = itemCode;
+        this.description = description;
+        this.packSize = packSize;
+        this.unitPrice = unitPrice;
+        this.qoh = qoh;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPackSize() {
+        return packSize;
+    }
+
+    public void setPackSize(String packSize) {
+        this.packSize = packSize;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQoh() {
+        return qoh;
+    }
+
+    public void setQoh(int qoh) {
+        this.qoh = qoh;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDto{" +
+                "itemCode='" + itemCode + '\'' +
+                ", description='" + description + '\'' +
+                ", packSize='" + packSize + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", qoh=" + qoh +
+                '}';
+    }
+}
