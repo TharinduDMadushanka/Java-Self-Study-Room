ef34f6e52a3945c20c5009b0e92a7066f687971f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-04 20:18:35 +0530

    create CustomerDto

--- Layered Architecture/src/edu/practice/Main.java
+++ Layered Architecture/src/edu/practice/Main.java
@@ -1,9 +1,11 @@
 package edu.practice;
 
+import edu.practice.view.CustomerView;
 import edu.practice.view.ItemView;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new ItemView().setVisible(true);
+        //new ItemView().setVisible(true);
+        new CustomerView().setVisible(true);
     }
 }
