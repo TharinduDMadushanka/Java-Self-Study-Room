b742859d46c9f930b236fb7e51b2fec8ba646b70
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-04 21:22:34 +0530

    Creating an Interface

---
+++ Practice/OOP/src/Interface/basics/Animal.java
@@ -0,0 +1,21 @@
+package Interface.basics;
+
+public interface Animal {
+
+     String name = "";
+//     int x; cant
+    
+    void makeSound(); // Abstract method
+    abstract void eat();
+    String getSound(); //Abstract method
+
+    default void sing() {
+
+    }
+
+    static void hi() {
+
+    }
+
+//    public Animal(){}  cant declare constructors
+}
